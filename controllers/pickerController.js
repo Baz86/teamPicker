2f17f881dba082195566e1f1b737dfad7d49b468
Baz86 <tombale@Toms-MacBook.home>
2017-06-24 14:10:41 +0100

    All body text to Avenir, brackets added to the skill of each player and comments for all three files

--- controllers/pickerController.js
+++ controllers/pickerController.js
@@ -1,7 +1,13 @@
 pickerTeamApp.controller("PickerController", ["$scope", function($scope) {
+
+	/* Here I am setting the value of my players, playersOne and playersTwo to empty arrays, 
+	ready to be populated by the values typed in to my form inputs using ng-model */
+
 	$scope.players = [];
 	$scope.playersOne = [];
 	$scope.playersTwo = [];
+
+	// below is a shuffle function for shuffling my players array
 
 	function shuffle(array) {
   			var currentIndex = array.length, temporaryValue, randomIndex;
@@ -22,6 +28,9 @@ pickerTeamApp.controller("PickerController", ["$scope", function($scope) {
   			return array;
 		}
 
+	/* Below is a function to add the totals of my playersOne and playersTwo arrays. This will be used
+	to display the totals of the even teams. This can  also be reused for other arrays of objects */
+
 	function teamTotal(team) {
 		var sum = 0;
 
@@ -32,17 +41,26 @@ pickerTeamApp.controller("PickerController", ["$scope", function($scope) {
 		return sum;
 	};
 
-	$scope.evenTeams = function() {
-			for (var i = 0; i < $scope.players.length; i += 1) {
+	/* The function below seperates my sorted players array into two evenly matched teams based on skill level.
+	 It loops through the array and assigns even indexes to the empty playersOne array and odd indexes to the 
+	 empty playersTwo array. */
+
+	$scope.evenTeams = function(team) {
+			for (var i = 0; i < team.length; i += 1) {
 				if ((i % 2)!= 0) {
-					$scope.playersOne.push($scope.players[i]);
+					$scope.playersOne.push(team[i]);
 				} else {
-					$scope.playersTwo.push($scope.players[i]);
+					$scope.playersTwo.push(team[i]);
 				}
 			};
 		}	
 	
-
+	/* The below function selects my two evenly matched teams. It first resets the playersOne and playerTwo
+	arrays to empty arrays. It then sorts my players array into ascending order based on skill level and runs
+	the sorted players array through my evenTeams function. Finally it adds up the total skill level for each
+	team using my teamTotal function and assigns them to two variables called playersOneTotal and playersTwoTotal
+	and displays the totals and team lists on the page. */
+	
 	$scope.pickTeams = function() {
 		$scope.playersOne = [];
 		$scope.playersTwo = [];
@@ -50,12 +68,18 @@ pickerTeamApp.controller("PickerController", ["$scope", function($scope) {
 			return a.skill - b.skill;
 		});
 
-		$scope.evenTeams();
+		$scope.evenTeams($scope.players);
 		$scope.playersOneTotal = teamTotal($scope.playersOne);
 		$scope.playersTwoTotal = teamTotal($scope.playersTwo);
 		$scope.showList = true;
 		$scope.total = true;
 	}
+
+	/* This function picks random teams. It first passes my players array into my shuffle function.
+	then creates a variable called cut and asigns it a value that is equal to half the arrays length. It then 
+	slices the shuffled players array using the cut variable to define the end point of the first and start
+	point of the second slice and assigns the results to the playersOne and playersTwo variables respectively.
+	Finally is shows the results on the page as two lists and hides the team totals. */
 
 	$scope.shuffleTeams = function() {
 		$scope.players = shuffle($scope.players);
@@ -66,33 +90,51 @@ pickerTeamApp.controller("PickerController", ["$scope", function($scope) {
 		$scope.total = false;
 	}
 
+	/* This function takes the value of the select picker and uses it in an if statement to see which team
+	size has been selected and then shows or hides the correct number of form inputs for that team total. */
+
 	$scope.changeTotal = function() {
 		
-		if ($scope.totalPlayers  === "12") {
+		if ($scope.totalPlayers  === "6") {
 			$scope.sixASide = true;
 			$scope.sevenASide = false;
 			$scope.eightASide = false;
 			$scope.nineASide = false;
-		} else if ($scope.totalPlayers === "14") {
+		} else if ($scope.totalPlayers === "7") {
 			$scope.sixASide = true;
 			$scope.sevenASide = true;
 			$scope.eightASide = false;
 			$scope.nineASide = false;
-		} else if ($scope.totalPlayers === "16") {
+		} else if ($scope.totalPlayers === "8") {
 			$scope.sixASide = true;
 			$scope.sevenASide = true;
 			$scope.eightASide = true;
 			$scope.nineASide = false;
-		} else if ($scope.totalPlayers === "18") {
+		} else if ($scope.totalPlayers === "9") {
 			$scope.sixASide = true;
 			$scope.sevenASide = true;
 			$scope.eightASide = true;
 			$scope.nineASide = true;
+		} else if ($scope.totalPlayers === "10") {
+			$scope.sixASide = true;
+			$scope.sevenASide = true;
+			$scope.eightASide = true;
+			$scope.nineASide = true;
+			$scope.tenASide = true;
+		} else if ($scope.totalPlayers === "11") {
+			$scope.sixASide = true;
+			$scope.sevenASide = true;
+			$scope.eightASide = true;
+			$scope.nineASide = true;
+			$scope.tenASide = true;
+			$scope.elevenASide = true;
 		} else {
 			$scope.sixASide = false;
 			$scope.sevenASide = false;
 			$scope.eightASide = false;
 			$scope.nineASide = false;
+			$scope.tenASide = false;
+			$scope.elevenASide = false;
 		}
 
 	}
